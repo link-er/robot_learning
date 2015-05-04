af7c04b1487d7e7b42d30a703c219b4c67f35895
link_er <link_er@bk.ru>
2015-05-04 17:22:47 +0200

    optimal policy output

--- GridWorld.java
+++ GridWorld.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 
 public class GridWorld {
     public static enum Move {LEFT, UP, RIGHT, DOWN};
@@ -18,15 +20,11 @@ public class GridWorld {
             rewardGrid[4][7] = -20; rewardGrid[5][7] = -20; rewardGrid[6][1] = -20; rewardGrid[6][2] = -20;
             rewardGrid[6][3] = -20; rewardGrid[6][5] = -20; rewardGrid[6][6] = -20;
             for (int i = 7; i < 9; i++)
-                for (int j = 1; j < 6; j++)
+                for (int j = 1; j <= 6; j++)
                     rewardGrid[i][j] = 5;
             for (int i = 0; i < 9; i++)
                 for (int j = 0; j < 9; j++) {
                     valueGrid[i][j] = initVal;
-                    if(i==2 && j==0)
-                        valueGrid[i][j] = 100;
-                    if(rewardGrid[i][j]==-20)
-                        valueGrid[i][j] = -20;
                 }
         }
     }
@@ -48,6 +46,50 @@ public class GridWorld {
                     moveProbabilities[i][j][3] = downProbability;
                 }
         }
+
+        public void output() {
+            Move currentMove;
+            for(int i=0; i<9; i++) {
+                for(int j=0; j<9; j++) {
+                    if(i==2 && j==0) {
+                        System.out.print("* ");
+                        continue;
+                    }
+                    currentMove = detectMove(i, j);
+                    if(currentMove == null)
+                        System.out.print("_ ");
+                    else
+                        switch(currentMove) {
+                        case LEFT: {
+                            System.out.print("< ");
+                            break;
+                        }
+                        case UP: {
+                            System.out.print("^ ");
+                            break;
+                        }
+                        case RIGHT: {
+                            System.out.print("> ");
+                            break;
+                        }
+                        case DOWN: {
+                            System.out.print("v ");
+                            break;
+                        }
+                        }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        public Move detectMove(int i, int j) {
+            if(moveProbabilities[i][j][Move.LEFT.ordinal()] == 1) return Move.LEFT;
+            if(moveProbabilities[i][j][Move.UP.ordinal()] == 1) return Move.UP;
+            if(moveProbabilities[i][j][Move.RIGHT.ordinal()] == 1) return Move.RIGHT;
+            if(moveProbabilities[i][j][Move.DOWN.ordinal()] == 1) return Move.DOWN;
+            return null;
+        }
     }
 
     private Grid grid;
@@ -62,15 +104,13 @@ public class GridWorld {
         grid = new Grid(initVal);
         policy = new Policy(leftProbability, upProbability, rightProbability, downProbability);
     }
+    public GridWorld(double initVal, Policy policy) {
+        grid = new Grid(initVal);
+        this.policy = policy;
+    }
 
     private int reward(int i, int j) {
         return grid.rewardGrid[i][j];
-    }
-    private int reward(int iPrev, int jPrev, int i, int j) {
-        if(iPrev == i && jPrev == j)
-            return -10;
-        else
-            return grid.rewardGrid[i][j];
     }
 
     public double value(int i, int j) {
@@ -131,14 +171,97 @@ public class GridWorld {
         int reward;
         for(Move a : Move.values()) {
             nextState = move(i, j, a);
-            reward = reward(i, j, nextState[0], nextState[1]);
-            if(reward == -20) {
-                nextState[0] = i;
-                nextState[1] = j;
+            if(nextState[0]==i && nextState[1]==j)
+                reward = -10;
+            else {
+                reward = reward(nextState[0], nextState[1]);
+                if(reward == -20) {
+                    nextState[0] = i;
+                    nextState[1] = j;
+                }
             }
             stateValue += policyMove(i, j, a) * (reward + 0.9 * value(nextState[0], nextState[1]));
         }
         return stateValue;
+    }
+
+    public double getStateMaximumValue(int i, int j) {
+        if(i==2 && j==0)
+            return 100;
+        if(reward(i, j)==-20)
+            return -20;
+
+        double stateActionValue;
+        int[] nextState;
+        int reward;
+        double maximumValue = -Double.MAX_VALUE;
+        for(Move a : Move.values()) {
+            nextState = move(i, j, a);
+            if(nextState[0]==i && nextState[1]==j)
+                reward = -10;
+            else {
+                reward = reward(nextState[0], nextState[1]);
+                if(reward == -20) {
+                    nextState[0] = i;
+                    nextState[1] = j;
+                }
+            }
+            stateActionValue = reward + 0.9 * value(nextState[0], nextState[1]);
+            if(stateActionValue > maximumValue)
+                maximumValue = stateActionValue;
+        }
+        return maximumValue;
+    }
+
+    public Policy computePolicy() {
+        grid.valueGrid[2][0] = Double.MAX_VALUE;
+        Policy result = new Policy();
+        double maximum;
+        double value;
+        for(int i=0; i<9; i++)
+            for(int j=0; j<9; j++) {
+                maximum = Double.MIN_VALUE;
+                if(i==2 && j==0)
+                    continue;
+                if(reward(i, j)==-20)
+                    continue;
+
+                //check neighbors
+                if(j>0) {
+                    value = value(i, j-1);
+                    if(value > maximum) {
+                        Arrays.fill(result.moveProbabilities[i][j], 0);
+                        result.moveProbabilities[i][j][Move.LEFT.ordinal()] = 1;
+                        maximum = value;
+                    }
+                }
+                if(i>0){
+                    value = value(i-1, j);
+                    if(value > maximum) {
+                        Arrays.fill(result.moveProbabilities[i][j], 0);
+                        result.moveProbabilities[i][j][Move.UP.ordinal()] = 1;
+                        maximum = value;
+                    }
+                }
+
+                if(j<8) {
+                    value = value(i, j+1);
+                    if(value > maximum) {
+                        Arrays.fill(result.moveProbabilities[i][j], 0);
+                        result.moveProbabilities[i][j][Move.RIGHT.ordinal()] = 1;
+                        maximum = value;
+                    }
+                }
+                if(i<8) {
+                    value = value(i+1, j);
+                    if(value > maximum) {
+                        Arrays.fill(result.moveProbabilities[i][j], 0);
+                        result.moveProbabilities[i][j][Move.DOWN.ordinal()] = 1;
+                        maximum = value;
+                    }
+                }
+            }
+        return result;
     }
 
     public void setValues(double[][] temp) {
@@ -167,7 +290,7 @@ public class GridWorld {
         double[][] tempGrid = new double[9][9];
         double maximum = 0.0;
         double delta;
-        gw.printValues();
+        //gw.printValues();
         do {
             maximum = 0;
             for (int i = 0; i < 9; i++)
@@ -178,13 +301,63 @@ public class GridWorld {
                         maximum = delta;
                 }
             gw.setValues(tempGrid);
-            gw.printValues();
+            //gw.printValues();
 //            try {
 //                Thread.sleep(1000);
 //            } catch (InterruptedException e) {
-//                // TODO Auto-generated catch block
 //                e.printStackTrace();
 //            }
-        } while (Math.abs(maximum - epsilon) > 0.001);
+        } while (maximum > epsilon);
+        gw.printValues();
+
+        GridWorld gw1 = new GridWorld(initValue, leftProbability, upProbability, rightProbability, downProbability);
+        //gw1.printValues();
+        do {
+            maximum = 0;
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++) {
+                    tempGrid[i][j] = gw1.getStateMaximumValue(i, j);
+                    delta = Math.abs(tempGrid[i][j] - gw1.value(i, j));
+                    if (delta > maximum)
+                        maximum = delta;
+                }
+            gw1.setValues(tempGrid);
+            //gw1.printValues();
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        } while (maximum > epsilon);
+        gw1.printValues();
+        Policy optimal = gw1.computePolicy();
+        optimal.output();
+
+        GridWorld gw2 = new GridWorld(initValue, optimal);
+        //TODO
+        //make policy values to non-determenistic
+        //0.6 is what now 1
+        //two diagonal 0.2 depending on what is 0.6
+        //gw2.printValues();
+        do {
+            maximum = 0;
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++) {
+                    tempGrid[i][j] = gw2.getStateMaximumValue(i, j);
+                    delta = Math.abs(tempGrid[i][j] - gw2.value(i, j));
+                    if (delta > maximum)
+                        maximum = delta;
+                }
+            gw2.setValues(tempGrid);
+            //gw2.printValues();
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        } while (maximum > epsilon);
+        gw2.printValues();
+        Policy optimalNonDetermenistic = gw2.computePolicy();
+        optimalNonDetermenistic.output();
     }
 }
