c47fa7bfbc8d92282456f14ccdf7364573a77217
Mariia Rybalka <mari1rybalka@gmail.com>
2015-05-30 18:47:15 +0200

    pole cart second task

--- PoleCart.java
+++ PoleCart.java
@@ -14,23 +14,17 @@ public class PoleCart {
         state[3]= 0.3;
     }
 
-    public void discreteTimeSimulation(){
+    public void calculateState(){
         double secDerPos = 0.0, secDerAngl = 0.0, prevAngle=0.0, prevAngVel=0.0, prevVel=0.0;
-        int t=0;
-        for(t=0;t<100;t++){
-            printStateOut(t);
-            prevAngle = state[2];
-            prevAngVel = state[3];
-            prevVel = state[1];
-            secDerAngl = secondDerivAngl(prevAngle,prevAngVel);
-            secDerPos = secondDerivPos(prevAngle,prevAngVel,secDerAngl);
-
-            state[3] += secDerAngl*0.01 + noise[3];
-            state[2] += prevAngVel*0.01 + noise[2];
-            state[1] += secDerPos*0.01 + noise[1];
-            state[0] += prevVel*0.01 + noise[0];
-        }
-        printStateOut(t);
+        prevAngle = state[2];
+        prevAngVel = state[3];
+        prevVel = state[1];
+        secDerAngl = secondDerivAngl(prevAngle,prevAngVel);
+        secDerPos = secondDerivPos(prevAngle,prevAngVel,secDerAngl);
+        state[3] += secDerAngl*0.01 + noise[3];
+        state[2] += prevAngVel*0.01 + noise[2];
+        state[1] += secDerPos*0.01 + noise[1];
+        state[0] += prevVel*0.01 + noise[0]; 
     }
 
     public void printStateOut(int t){
@@ -48,10 +42,54 @@ public class PoleCart {
         return (g*Math.sin(angle)*(mc+mp)-Math.cos(angle)*(F+mp*l*Math.pow(angleVel, 2)*Math.sin(angle)))/((4.0/3.0)*l*
                 (mc+mp)-mp*l*Math.pow(Math.cos(angle), 2));
     }
-    public static void main(String[] args){
-
-        PoleCart pc = new PoleCart();
-        pc.discreteTimeSimulation();
+    
+    public void calculateForce(double k1,double k2, double k3, double k4){
+        double policyForce = 0.0;
+        policyForce = k1*state[0]+k2*state[1]+k3*state[2]+k4*state[3];
+        if(policyForce>-20)
+            F = policyForce;
+        else
+            F = -20;
+        if(F>20)
+            F = 20;
     }
+    
+    public boolean checkTarget(){
+        return (Math.abs(state[0])<=0.1 && Math.abs(state[2])<=0.1);
+    }
+    
+    public boolean checkFail(){
+        return (Math.abs(state[0])>2.4 && Math.abs(state[2])>0.7);
+    }
+    public static void main(String[] args){
+        int t = 0;
+        PoleCart pc = new PoleCart();
+        for(t=0;t<100;t++){
+            pc.printStateOut(t);
+            pc.calculateState();
+    } 
+        pc.printStateOut(t);
+        
+        PoleCart pc1 = new PoleCart();
+        double k1 = 0.01, k2 = -1.0, k3 = 0.02, k4 = -0.4;
+        t = 0;
+        int reward = 0;
+        while (true) {
+            pc1.printStateOut(t);
+            t++;
+            pc1.calculateForce(k1, k2, k3, k4);
+            pc1.calculateState();
+            if (pc1.checkTarget())
+                reward += 0;
+            else if (pc1.checkFail()) {
+                reward += -2 * (1000 - t);
+                break;
+            } else
+                reward += -1;
+            System.out.println(reward);
 
-}
+        }
+        System.out.println("Final reward: "+reward);
+    
+
+    }}
