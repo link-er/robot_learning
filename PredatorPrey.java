e924203c8e387c56f1d333c979e2806bc09e6bc3
link_er <link_er@bk.ru>
2015-05-31 15:09:10 +0200

    predator prey

--- PredatorPrey.java
+++ PredatorPrey.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 
 
 public class PredatorPrey {
-    public static final int PERIOD = 50;
-    public static final int START_X = 100;
-    public static final int START_Y = 100;
+    public static final int PERIOD = 100;
+    public static final int START_X = 500;
+    public static final int START_Y = 40;
 
     private double a,b,c,d,e,f,g,h;
     public PredatorPrey(double[] params) {
@@ -41,21 +41,10 @@ public class PredatorPrey {
     }
 
     public static void main(String[] args) {
-        //a, b, c, d, e, f, g, h
-        double[] params = new double[6];
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a");
-        params[0] = in.nextDouble();
-        System.out.println("Enter b");
-        params[1] = in.nextDouble();
-        System.out.println("Enter c");
-        params[2] = in.nextDouble();
-        System.out.println("Enter d");
-        params[3] = in.nextDouble();
-        System.out.println("Enter e");
-        params[4] = in.nextDouble();
-        System.out.println("Enter f");
-        params[5] = in.nextDouble();
+        //a, b, c, d, e, f
+        //x(i+1) = x(i)+a∗x(i)+b∗y(i)+e∗x(i)∗x(i)
+        //y(i+1) = y(i)+c∗x(i)+d∗y(i)+f ∗y(i)∗y(i)
+        double[] params = { 0.0, -2.0, 0.5, 0.5, -0.1, -0.1 };
 
         PredatorPrey pp = new PredatorPrey(params);
         double currentX = START_X, currentY = START_Y, tempX;
@@ -70,26 +59,11 @@ public class PredatorPrey {
             currentY = pp.evaluateY(currentY, tempX);
         }
 
-        //a, b, c, d, e, f, g, h
-        double[] paramsExt = new double[8];
-        System.out.println("Enter a");
-        paramsExt[0] = in.nextDouble();
-        System.out.println("Enter b");
-        paramsExt[1] = in.nextDouble();
-        System.out.println("Enter c");
-        paramsExt[2] = in.nextDouble();
-        System.out.println("Enter d");
-        paramsExt[3] = in.nextDouble();
-        System.out.println("Enter e");
-        paramsExt[4] = in.nextDouble();
-        System.out.println("Enter f");
-        paramsExt[5] = in.nextDouble();
-        System.out.println("Enter g");
-        paramsExt[6] = in.nextDouble();
-        System.out.println("Enter h");
-        paramsExt[7] = in.nextDouble();
 
-        in.close();
+        //a, b, c, d, e, f, g, h
+        //x(i+1) = x(i)+a∗x(i)+b∗y(i)+e∗x(i)∗x(i)+g∗x(i)∗y(i)
+        //y(i+1) = y(i)+c∗x(i)+d∗y(i)+f ∗y(i)∗y(i)+h∗x(i)∗y(i)
+        double[] paramsExt = { 0.09, 0.0, 0.0, 0.0, 0.0, -0.03, -0.004, 0.0001 };
 
         PredatorPrey ppExt = new PredatorPrey(paramsExt);
         currentX = START_X;
