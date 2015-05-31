08cb45957befb7d0ef6596b1a3154574af56569b
link_er <link_er@bk.ru>
2015-05-31 15:03:07 +0200

    doc for sheet 6

--- PoleCart.java
+++ PoleCart.java
@@ -1,3 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 public class PoleCart {
     private double l = 0.75, mc = 1.0, mp = 0.5, g = 9.81;
@@ -8,6 +13,9 @@ public class PoleCart {
     public PoleCart(){
         F = 0;
         state = new double[4];
+    }
+
+    public void setToInitial() {
         state[0]=1;
         state[1]=-0.2;
         state[2]= -0.2;
@@ -24,7 +32,7 @@ public class PoleCart {
         state[3] += secDerAngl*0.01 + noise[3];
         state[2] += prevAngVel*0.01 + noise[2];
         state[1] += secDerPos*0.01 + noise[1];
-        state[0] += prevVel*0.01 + noise[0]; 
+        state[0] += prevVel*0.01 + noise[0];
     }
 
     public void printStateOut(int t){
@@ -42,7 +50,7 @@ public class PoleCart {
         return (g*Math.sin(angle)*(mc+mp)-Math.cos(angle)*(F+mp*l*Math.pow(angleVel, 2)*Math.sin(angle)))/((4.0/3.0)*l*
                 (mc+mp)-mp*l*Math.pow(Math.cos(angle), 2));
     }
-    
+
     public void calculateForce(double k1,double k2, double k3, double k4){
         double policyForce = 0.0;
         policyForce = k1*state[0]+k2*state[1]+k3*state[2]+k4*state[3];
@@ -53,20 +61,20 @@ public class PoleCart {
         if(F>20)
             F = 20;
     }
-    
+
     public boolean checkTarget(){
         return (Math.abs(state[0])<=0.1 && Math.abs(state[2])<=0.1);
     }
-    
+
     public boolean checkFail(){
         return (Math.abs(state[0])>2.4 || Math.abs(state[2])>0.7);
     }
+
     public int episode(double k1, double k2, double k3, double k4){
         int t = 0;
         int reward = 0;
         while (true) {
             t++;
-            
             calculateForce(k1, k2, k3, k4);
             calculateState();
             if (checkTarget())
@@ -76,67 +84,83 @@ public class PoleCart {
                 break;
             } else
                 reward += -1;
-           
-
         }
         return reward;
     }
+
     public static void main(String[] args){
         int t = 0;
+        //TASK1
         PoleCart pc = new PoleCart();
         for(t=0;t<100;t++){
             pc.printStateOut(t);
             pc.calculateState();
-    } 
+        }
         pc.printStateOut(t);
-        
-        PoleCart pc1 = new PoleCart();
-        double k1 = 0.01, k2 = -10.0, k3 = 0.102, k4 = -0.54;
+
+        //TASK2
+        pc.setToInitial();
+        double k1 = -1, k2 = 3, k3 = -1, k4 = 2;
+        //double k1=-485.44374999999945, k2=-513.5300000000018, k3=-230.34125000000085, k4=281.4124999999998;
         t = 0;
         int reward = 0;
         while (true) {
-            pc1.printStateOut(t);
+            pc.printStateOut(t);
             t++;
-            pc1.calculateForce(k1, k2, k3, k4);
-            pc1.calculateState();
-            if (pc1.checkTarget())
+            pc.calculateForce(k1, k2, k3, k4);
+            pc.calculateState();
+            try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("pole_output_2.dat", true)))) {
+                out.println(t + " " + pc.state[0] + " " + pc.state[1] + " " + pc.state[2] + " " + pc.state[3]);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (pc.checkTarget())
                 reward += 0;
-            else if (pc1.checkFail()) {
+            else if (pc.checkFail()) {
                 reward += -2 * (1000 - t);
                 break;
             } else
                 reward += -1;
-            System.out.println(reward);
-
+            //System.out.println(reward);
         }
         System.out.println("Final reward: "+reward);
-        
-        
-        PoleCart pc2 = new PoleCart();
-        double[] gradient = new double[4];
-        System.out.println("last polecart");
-        for(int i = 0;i<4;i++){
-        t = 0;
-        reward = 0;
-        
-        double epsilon = 0.01;
-        double alpha = 0.001;
-        reward = pc2.episode(k1, k2, k3, k4);
-        System.out.println(k1+" "+k2+" "+k3+" "+k4);
-        gradient[0] = (pc2.episode(k1+epsilon, k2, k3, k4)-reward)/epsilon;
-        gradient[1] = (pc2.episode(k1, k2+epsilon, k3, k4)-reward)/epsilon;
-        gradient[2] = (pc2.episode(k1, k2, k3+epsilon, k4)-reward)/epsilon;
-        gradient[3] = (pc2.episode(k1, k2, k3, k4+epsilon)-reward)/epsilon;
-        k1 += alpha*gradient[0];
-        k2 += alpha*gradient[1];
-        k3 += alpha*gradient[2];
-        k4 += alpha*gradient[3];
-        System.out.println(k1+" "+k2+" "+k3+" "+k4);
-        }
-        
-            
-        
-        
-    
 
-    }}
+        //TASK3
+        pc.setToInitial();
+        double[] gradient = new double[4];
+        double epsilon = 0.8;
+        double alpha = 0.001;
+        double maxReward = -2000;
+        double[] maxParams = new double[4];
+        for(int i = 0;i<5000;i++){
+            pc.setToInitial();
+            reward = pc.episode(k1, k2, k3, k4);
+            //System.out.println(k1+" "+k2+" "+k3+" "+k4+" "+reward);
+            if(reward > maxReward) {
+                maxReward = reward;
+                maxParams[0] = k1;
+                maxParams[1] = k2;
+                maxParams[2] = k3;
+                maxParams[3] = k4;
+            }
+            try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("pole_output_3.dat", true)))) {
+                out.println(i + " " + k1 + " " + k2 + " " + k3 + " " + k4 + " " + reward);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            pc.setToInitial();
+            gradient[0] = (pc.episode(k1+epsilon, k2, k3, k4)-reward)/epsilon;
+            pc.setToInitial();
+            gradient[1] = (pc.episode(k1, k2+epsilon, k3, k4)-reward)/epsilon;
+            pc.setToInitial();
+            gradient[2] = (pc.episode(k1, k2, k3+epsilon, k4)-reward)/epsilon;
+            pc.setToInitial();
+            gradient[3] = (pc.episode(k1, k2, k3, k4+epsilon)-reward)/epsilon;
+            k1 += alpha*gradient[0];
+            k2 += alpha*gradient[1];
+            k3 += alpha*gradient[2];
+            k4 += alpha*gradient[3];
+        }
+        System.out.println(maxParams[0]+" "+maxParams[1]+" "+maxParams[2]+" "+maxParams[3]+" "+maxReward);
+    }
+}
