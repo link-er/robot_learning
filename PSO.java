0cb53ce563bcfaf219afd87cbf0be9e4c0e4aa09
mari <mari1rybalka@gmail.com>
2015-07-05 18:20:56 +0200

    PSO final

--- PSO.java
+++ PSO.java
@@ -1,25 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class PSO {
 	private int dimension;// dimension
 	private int numberOfParticles;// number of particles
-	private double omega=1.0;
-	private double alpha=2.0;
-	private double beta=2.0;
+	private double omega = 1.0;
+	private double alpha = 2.0;
+	private double beta = 2.0;
 	private Particle[] particles;
 	private double[] globalBestPosition;
 	private double globalBestFitness;
 
-	public PSO(int N, int P) {
+	public PSO(int N, int P, int initType) {
 		dimension = N;
 		numberOfParticles = P;
 		globalBestFitness = Double.MAX_VALUE;
 		particles = new Particle[numberOfParticles];
 		globalBestPosition = new double[dimension];
-		for (int i = 0; i < particles.length; i++) {
-			particles[i] = new Particle(dimension);
-			particles[i].intitializeParticle();
-		}
+		if (initType == 1)
+			initialize1();
+		else
+			initialize2();
 		for (Particle particle : particles) {
 			if (particle.getFitness() < globalBestFitness) {
 				globalBestFitness = particle.getFitness();
@@ -28,35 +32,125 @@ public class PSO {
 			}
 		}
 	}
-	public boolean stopCriteria(int count){
+
+	public void initialize1() {
+		for (int i = 0; i < particles.length; i++) {
+			particles[i] = new Particle(dimension);
+			particles[i].intitializeParticle();
+		}
+	}
+
+	public void initialize2() {
+		for (int i = 0; i < particles.length; i++) {
+			particles[i] = new Particle(dimension);
+			particles[i].intitializeParticleHyperCube();
+		}
+	}
+
+	public boolean stopCriteria(int count) {
 		int stop = 100;
-		System.out.println(globalBestFitness);
-		return count==stop;}
+		return count == stop;
+	}
 
 	public void algorithm() {
-		
+
 		int count = 0;
 		while (!stopCriteria(count)) {
 			for (Particle particle : particles) {
 				particle.updateVelocity(omega, alpha, beta, globalBestPosition);
-				particle.updatePostion();
-				particle.fitnessFunction();
+				particle.updatePostion(dimension);
+				particle.fitnessFunction(dimension);
 			}
 			for (Particle particle : particles) {
 				if (particle.getFitness() < globalBestFitness) {
 					globalBestFitness = particle.getFitness();
 					globalBestPosition = Arrays.copyOf(particle.getPosition(),
 							globalBestPosition.length);
-					
+
 				}
 			}
 			count++;
 		}
+		System.out.println("Global best fitness: ");
+		System.out.println(globalBestFitness);
+		System.out.println("Global best Position: ");
+		for (double coordinate : globalBestPosition)
+			System.out.printf("%.2f ", coordinate);
+		System.out.println();
 	}
 
-	public static void main(String[] args) {
-		PSO pso = new PSO(2, 10);
-		pso.algorithm();
+	public void algorithmPrintHosakiToFile() throws IOException {
+		File file = new File("Hosaki.dat");
+		PrintWriter pw = new PrintWriter(file);
+		int count = 0;
+		while (!stopCriteria(count)) {
+			for (Particle particle : particles) {
+				particle.updateVelocity(omega, alpha, beta, globalBestPosition);
+				particle.updatePostion(dimension);
+				particle.fitnessFunction(dimension);
+			}
+			for (Particle particle : particles) {
+				if (particle.getFitness() < globalBestFitness) {
+					globalBestFitness = particle.getFitness();
+					globalBestPosition = Arrays.copyOf(particle.getPosition(),
+							globalBestPosition.length);
+				}
+			}
+			double[] coordinates = { 0.0, 0.0 };
+			for (int i = 0; i < 8; i++) {
+				coordinates = Arrays.copyOf(particles[i].getPosition(),
+						dimension);
+				pw.printf("%.5f  %.5f  ", coordinates[0], coordinates[1]);
 
+			}
+			count++;
+			pw.println();
+		}
+		pw.flush();
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		int N;
+		int P = 100;
+		int initType;
+		initType = 1;// Initialize at random
+		System.out
+				.println("---------------Random Initialization----------------------");
+		N = 1;// univariate function
+		System.out
+				.println("---------------Univariate function------------------------");
+		PSO pso = new PSO(N, P, initType);
+		pso.algorithm();
+		N = 2;// Hosaki function
+		System.out
+				.println("---------------Hosaki function----------------------------");
+		PSO pso2 = new PSO(N, P, initType);
+		pso2.algorithm();
+		N = 10;// Rastrigin function
+		System.out
+				.println("---------------Rastrigin function-------------------------");
+		PSO pso3 = new PSO(N, P, initType);
+		pso3.algorithm();
+		System.out
+				.println("---------------Initialization Within Hypercube------------");
+		initType = 2;// Initialize within small hypercube 0.05;
+		N = 1;// univariate function
+		System.out
+				.println("---------------Univariate function------------------------");
+		PSO pso4 = new PSO(N, P, initType);
+		pso4.algorithm();
+		N = 2;// Hosaki function
+		System.out
+				.println("---------------Hosaki function----------------------------");
+		PSO pso5 = new PSO(N, P, initType);
+		pso5.algorithm();
+		PSO psoFile = new PSO(N, P, initType);
+		psoFile.algorithmPrintHosakiToFile();
+		N = 10;// Rastrigin function
+		System.out
+				.println("---------------Rastrigin function-------------------------");
+		PSO pso6 = new PSO(N, P, initType);
+		pso6.algorithm();
 	}
 }
--- Particle.java
+++ Particle.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.math.*;
 
 
 public class Particle {
@@ -26,23 +27,79 @@ public class Particle {
 		}
 	}
 	
+	public void intitializeParticleHyperCube(){
+		for(int i=0; i<position.length; i++){
+			position[i] =Math.random()/20;
+			velocity[i] = -10+Math.random()*20;
+			personalBest[i] = position[i];
+		}
+	}
+	
+	
 	public void updateVelocity(double w, double a, double b, double[] globalBest){
 		double R = Math.random();
 		for (int i = 0; i < velocity.length; i++)
 			velocity[i] = w*velocity[i] + a*R*(-position[i]+personalBest[i]) + b*R*(-position[i]+globalBest[i]);	
 	}
 
-	public void updatePostion() {
-		for (int i = 0; i < position.length; i++)
-		{
+	public void updatePostion(int dimension) {
+		//System.out.println(currentFitness);
+		for (int i = 0; i < position.length; i++) {
 			position[i] = position[i] + velocity[i];
-			if(position[i]>10) position[i]=10;
-			if(position[i]<0) position[i]=0;
+			switch (dimension) {
+			case 1: {
+				if (position[i] > 10)
+					position[i] = 10;
+				if (position[i] < 0)
+					position[i] = 0;
+				break;
+			}
+			case 10: {
+				if (position[i] > 5.12)
+					position[i] = 5.12;
+				if (position[i] < -5.12)
+					position[i] = -5.12;
+				break;
+			}
+			case 2: {
+				if (position[i] > 10)
+					position[i] = 10;
+				if (position[i] < 0)
+					position[i] = 0;
+				break;
+			}
+			default:
+				break;
+			}
 		}
 	}
 	
-	public void fitnessFunction(){
-		currentFitness = position[0]*Math.cos(2*position[0])+position[0]*Math.sin(position[0]);
+	public void fitnessFunction(int fitnessType) {
+		switch (fitnessType) {
+		case 1:
+			currentFitness = position[0] * Math.cos(2 * position[0])
+					+ position[0] * Math.sin(position[0]);
+			break;
+		case 10: {
+			for (int i = 0; i < fitnessType; i++)
+				currentFitness += Math.pow(position[i], 2)
+						- 10*Math.cos(2.0 * Math.PI * position[i]);
+			currentFitness += 10 * fitnessType;
+			
+			break;
+		}
+		case 2: {
+			currentFitness = (1 - 8 * position[0] + 7 * Math.pow(position[0], 2)
+					- (7.0/ 3.0) * Math.pow(position[0], 3) + (1.0 / 4.0)
+					* Math.pow(position[0], 4))*Math.pow(position[1],2)*Math.exp(-position[1]);
+			
+			break;
+		}
+		default:
+			currentFitness=0;
+		//	break;
+		}
+
 		setPersonalBest(currentFitness);
 	}
 	
